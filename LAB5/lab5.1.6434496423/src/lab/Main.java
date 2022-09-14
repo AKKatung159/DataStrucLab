1f53f340e4b6d24967c29e4c432d4426b767782d
AKKatung159 <6434496423@student.chula.ac.th>
2022-09-14 16:14:33 +0700

    fix lab5.1

--- LAB5/lab5.1.6434496423/src/lab/Main.java
+++ LAB5/lab5.1.6434496423/src/lab/Main.java
@@ -10,19 +10,25 @@ public class Main {
     public static String reverse(String s) {
         String backR ="";
         String back = s.substring(1,s.length());
+        String y="";
         backR += s.substring(0,1);
-        stack.push(new ARI("c",s,back,backR));
-        System.out.println("CALL");
+        stack.push(new ARI(s,"",""));
+        System.out.print("Call reverse(\""+s+"\")\n=== BOTTOM OF STACK ===");
         System.out.println(stack.toString());
+        ARI ari = (ARI) stack.peek();
+        ari.setBack(back);
         if (s.length()>1) {
-            String y=reverse(back)+backR;
+            y=reverse(back);
+            backR=y+backR;
             stack.pop();
-            System.out.println("RETURN");
+            System.out.print("Return from reverse(\""+s+"\")\n=== BOTTOM OF STACK ===");
+            ari = (ARI) stack.peek();
+            ari.setBackR(y);
             System.out.println(stack.toString());
-            return y;
+            return backR;
         }
         else {
-            System.out.println("RETURN222");
+            System.out.print("Return from reverse(\""+s+"\")\n=== BOTTOM OF STACK ===");
             System.out.println(stack.toString());
             return backR;
         }
